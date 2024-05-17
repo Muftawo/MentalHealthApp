30bb0c5f43e90689652121105c91abfc624727b7
Muftawo <muftawo.omar@berkeley.edu>
2024-05-17 18:51:22 +0000

    moved home screen to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,75 +14,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[600],
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              // greetings row
-              GreetingsRow()
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class GreetingsRow extends StatelessWidget {
-  const GreetingsRow({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Hi, Mufti!",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "7th Sep 2024",
-              style: TextStyle(color: Colors.blue[100], fontSize: 10),
-            )
-          ],
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.blue[700],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          padding: const EdgeInsets.all(4),
-          child: const Icon(
-            Icons.notifications,
-            color: Colors.white,
-          ),
-        )
-      ],
     );
   }
 }
