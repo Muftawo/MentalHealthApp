0b2f68c450c64b82dc66dde89d285be363d541be
Muftawo <muftawo.omar@berkeley.edu>
2024-05-17 16:54:21 +0000

    stucture common colors

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MhColors {
+  MhColors._();
+  // App Basic Colors
+  static var primary = Colors.blue[700];
+  static var secondary = Colors.blue[500];
+
+  // Text
+  static const textPrimary = Colors.white;
+  static var textSecondary = Colors.blue[100];
+
+  //Icon
+  static const iconPrimary = Colors.white;
+}
